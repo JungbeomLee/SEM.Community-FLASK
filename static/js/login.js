597a29d3177cf47a5176eda7d42436b87e96bb51
JungBeomLee <jungbum05@naver.com>
2022-11-07 01:40:39 +0900

    REFACTOR(login) : Use fetch to posting form data success

--- static/js/login.js
+++ static/js/login.js
@@ -1,6 +1,26 @@
+function userLoginDataPost(loginId, password) {
+    let loginData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'useremail': loginId,
+            'password': password
+        }),
+        mode: 'no-cors'
+    };
+    fetch(`/login/post`, loginData)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data['useremail'])
+            document.getElementById('title').innerText = data['useremail']
+        })
+}
+
 function login() {
     const loginBtn = document.getElementById('login_btn');
-    const form = document.getElementById('form');
+    const form = document.getElementById('form');   
 
     loginBtn.addEventListener('click', (e) => {
         e.preventDefault(); // 기본 폼 동작 막기
@@ -10,22 +30,7 @@ function login() {
         let password = document.getElementById('password').value;
 
         console.log(JSON.stringify({ loginId, password }))
-        
-        let loginData = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: new URLSearchParams({
-                'useremail': loginId,
-                'password' : password
-            }),
-            mode : 'no-cors'
-        };
-            fetch(`/login/post`, loginData)
-            .then((res) => console.log('response', res))
-            .then((json) => {
-                console.log(json);
-            })
+
+        userLoginDataPost(loginId, password)
     });
 }
