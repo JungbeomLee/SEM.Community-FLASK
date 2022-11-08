d90df72cf0ea2cc139b621b48836142f4c2da613
JungBeomLee <jungbum05@naver.com>
2022-11-08 09:51:52 +0900

    REFACTOR(merge) : Bord merge

--- views/board/show_board/main.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-bp = Blueprint('main', __name__, url_prefix='/')
-
-@bp.route('/', methods=['GET', 'POST'])
-def main():
-    return render_template('main.html')
