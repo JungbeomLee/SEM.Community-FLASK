9a5f0d7f0d2935db4575e4943c092fc3302cfc17
JungbeomLee <jungbum05@naver.com>
2022-11-09 23:55:42 +0900

    REFACTOR(own user) : Js own user password check div remov

--- static/js/post_user_data.js
+++ static/js/post_user_data.js
@@ -36,21 +36,23 @@ function arrangePostData(data) {
         // 타이틀 설정
         document.getElementById('user_name').innerText = data['user_name'];
         // 이미지 링크 설정
-        document.getElementById('profilePic').src = data['user_profile_image_link']
+        document.getElementById('profilePic').src = data['user_profile_image_link'];
         // 프로필 이미지 유무 확인
         if (data['check_user_profile_image_name_check'] = false) {
             // 프로필 이미지 없을경우 openPopup_no_profile_image_pass(); 넣기
-            document.getElementById('user_image_label').innerHTML = '<label onclick="openPopup_no_profile_image_pass();" for="newProfilePhoto" class="upload-file-block"><div class="text-center"><div class="mb-2"><i class="fa fa-camera fa-2x"></i></div><div class="text-uppercase">Update <br /> Profile Photo</div></div></label>'
+            document.getElementById('user_image_label').innerHTML = '<label onclick="openPopup_no_profile_image_pass();" for="newProfilePhoto" class="upload-file-block"><div class="text-center"><div class="mb-2"><i class="fa fa-camera fa-2x"></i></div><div class="text-uppercase">Update <br /> Profile Photo</div></div></label>';
         } else {
             // 프로필 이미지 있을경우 openPopup_have_profile_image(); 넣기
-            document.getElementById('user_image_label').innerHTML = '<label onclick="openPopup_have_profile_image();" for="newProfilePhoto" class="upload-file-block"><div class="text-center"><div class="mb-2"><i class="fa fa-camera fa-2x"></i></div><div class="text-uppercase">Update <br /> Profile Photo</div></div></label>'
+            document.getElementById('user_image_label').innerHTML = '<label onclick="openPopup_have_profile_image();" for="newProfilePhoto" class="upload-file-block"><div class="text-center"><div class="mb-2"><i class="fa fa-camera fa-2x"></i></div><div class="text-uppercase">Update <br /> Profile Photo</div></div></label>';
         }
         // 수정 가능한 데이터 입력 form
-        document.getElementById('form_input_change_user_data_div').innerHTML = '<div class="form-group"><input type="text" class="form-control" id="change_nickname" value="" placeholder="닉네임" name="change_nickname" required /></div><div class="form-group"><label for="change_profile"></label><input type="text" class="form-control" id="change_profile" value="" placeholder="자기소개" name="change_profile" required /></div><button type="submit" class="btn btn-primary">확인</button></form>'
+        document.getElementById('form_input_change_user_data_div').innerHTML = '<div class="form-group"><input type="text" class="form-control" id="change_nickname" value="" placeholder="닉네임" name="change_nickname" required /></div><div class="form-group"><label for="change_profile"></label><input type="text" class="form-control" id="change_profile" value="" placeholder="자기소개" name="change_profile" required /></div><button type="submit" class="btn btn-primary">확인</button></form>';
         // 닉네임 기존값 넣기 
-        document.getElementById('change_nickname').value = data['user_nickname']
+        document.getElementById('change_nickname').value = data['user_nickname'];
         // 자기소개 기존값 넣기
-        document.getElementById('change_profile').value = data['user_profile']
+        document.getElementById('change_profile').value = data['user_profile'];
+        // 비밀번호 입력창 제거
+        document.getElementById('form').innerHTML = '';
     }
 
 }
